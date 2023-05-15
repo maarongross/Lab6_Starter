5ddf3a0f583efe959901efb0a0a1ed3b4ddbcd6e
maarongross <97714828+maarongross@users.noreply.github.com>
2023-05-14 21:40:43 -0700

    expose progress

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -16,8 +16,81 @@ class RecipeCard extends HTMLElement {
     const styleElement = document.createElement('style');
     console.log(styleElement);
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
-    const templateStyle = document.querySelector('style');
-    styleElement.innerHTML += templateStyle.innerHTML;
+    styleElement.textContent = `* {
+      font-family: sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+  
+    a {
+      text-decoration: none;
+    }
+  
+    a:hover {
+      text-decoration: underline;
+    }
+  
+    article {
+      align-items: center;
+      border: 1px solid rgb(223, 225, 229);
+      border-radius: 8px;
+      display: grid;
+      grid-template-rows: 118px 56px 14px 18px 15px 36px;
+      height: auto;
+      row-gap: 5px;
+      padding: 0 16px 16px 16px;
+      width: 178px;
+    }
+  
+    div.rating {
+      align-items: center;
+      column-gap: 5px;
+      display: flex;
+    }
+  
+    div.rating>img {
+      height: auto;
+      display: inline-block;
+      object-fit: scale-down;
+      width: 78px;
+    }
+  
+    article>img {
+      border-top-left-radius: 8px;
+      border-top-right-radius: 8px;
+      height: 118px;
+      object-fit: cover;
+      margin-left: -16px;
+      width: calc(100% + 32px);
+    }
+  
+    p.ingredients {
+      height: 32px;
+      line-height: 16px;
+      padding-top: 4px;
+      overflow: hidden;
+    }
+  
+    p.organization {
+      color: black !important;
+    }
+  
+    p.title {
+      display: -webkit-box;
+      font-size: 16px;
+      height: 36px;
+      line-height: 18px;
+      overflow: hidden;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+  
+    p:not(.title),
+    span,
+    time {
+      color: #70757A;
+      font-size: 12px;
+    }`;
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
     shadow.appendChild(articleElement);
     shadow.appendChild(styleElement);
@@ -50,6 +123,7 @@ class RecipeCard extends HTMLElement {
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
     shadowArticle = this.shadowRoot.children[0];
+    console.log(shadowArticle);
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
@@ -60,4 +134,4 @@ class RecipeCard extends HTMLElement {
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
-customElements.define('recpie-card', RecipeCard);
+customElements.define('recipe-card', RecipeCard);
